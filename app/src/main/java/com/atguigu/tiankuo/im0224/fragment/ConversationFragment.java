5f7d3920d67ae22d238388b22579d7b96a10bbb7
tiankuo-android <1226147264@qq.com>
2017-07-04 20:26:45 +0800

    修复设置页面BUG

--- app/src/main/java/com/atguigu/tiankuo/im0224/fragment/ConversationFragment.java
+++ app/src/main/java/com/atguigu/tiankuo/im0224/fragment/ConversationFragment.java
@@ -6,7 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.atguigu.tiankuo.im0224.R;
 
 /**
  * 作者：田阔
@@ -17,9 +18,8 @@ public class ConversationFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView textView =  new TextView(getActivity());
-        textView.setText("1111");
-        return textView;
+        View view = View.inflate(getContext(), R.layout.fragment_conversation,null);
+        return view;
 
     }
 }
